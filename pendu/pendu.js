7e01603a2a4227a6cad0b051d37d84dded1609b1
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-02-03 14:45:59 +0100

    Update pendu.js

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -2,13 +2,13 @@ mots = ["CHIEN", "CHAT", "MAISON", "JARDIN", "VOITURE", "SOLEIL"];
 secret = mots[Math.floor(Math.random() * mots.length)];
 longueur = secret.length;
 étapes = [
-	" +---+\n |   |\n     |\n     |\n     |\n     |\n",
-	" +---+\n |   |\n ☻   |\n     |\n     |\n     |\n",
-	" +---+\n |   |\n ☻   |\n |   |\n     |\n     |\n",
-	" +---+\n |   |\n ☻   |\n/|   |\n     |\n     |\n",
-	" +---+\n |   |\n ☻   |\n/|\\  |\n     |\n     |\n",
-	" +---+\n |   |\n ☻   |\n/|\\  |\n/    |\n     |\n",
-	" +---+\n |   |\n ☻   |\n/|\\  |\n/ \\  |\n     |\n"
+	" ┌───┐\n │   │\n     │\n     │\n     │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n     │\n     │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n │   │\n     │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n╭┤   │\n     │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n╭┼╮  │\n     │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n╭┼╮  │\n╱    │\n     │\n",
+	" ┌───┐\n │   │\n ☻   │\n╭┼╮  │\n╱ ╲  │\n     │\n"
 ];
 essaisRestants = 6;
 trouvées = 0;
@@ -17,46 +17,41 @@ for (var i = 0; i < longueur; i++) {affiché += "_ ";}
 
 function initialiser() {
 	mot.textContent = affiché.trim();
-	button.addEventListener(
-		"click",
-		function() {
-			tentative = input.value.toUpperCase();
-			if (tentative.length !== 1 || !tentative.match(/[a-z]/i)) {
-				message.textContent = "Veuillez entrer une lettre de l'alphabet.";
-			} else {
-				nouveauAffiché = "";
-				trouvée = false;
-				for (var i = 0; i < longueur; i++) {
-					if (secret[i] === tentative) {
-						nouveauAffiché += tentative + " ";
-						trouvée = true;
-						trouvées++;
-					} else {
-						nouveauAffiché += mot.textContent[i * 2] + " ";
-					}
-				}
-				mot.textContent = nouveauAffiché.trim();
-				input.value = "";
-				input.focus();
-				if (trouvée) {
-					if (trouvées === longueur) {
-						message.textContent = "Félicitations ! Vous avez deviné le mot.";
-						input.disabled = true;
-						button.disabled = true;
-					} else {
-						message.textContent = "Bonne devinette !";
-					}
-				} else {
-					message.textContent = "Ce n'est pas une bonne lettre. Essayez encore !";
-					essaisRestants--;
-					pre.textContent = étapes[6 - essaisRestants];
-					if (essaisRestants === 0) {
-						message.textContent = "Dommage, vous avez perdu. Le mot était : " + secret;
-						input.disabled = true;
-						button.disabled = true;
-					}
-				}
+	span.innerText = étapes[0];
+}
+
+function essayer() {
+	tentative = input.value.toUpperCase();
+	if (tentative.length !== 1 || !tentative.match(/[a-z]/i)) {
+		message.textContent = "Veuillez entrer une lettre de l'alphabet.";
+	} else {
+		nouveauAffiché = "";
+		trouvée = false;
+		for (var i = 0; i < longueur; i++) {
+			if (secret[i] === tentative) {
+				nouveauAffiché += tentative + " ";
+				trouvée = true;
+				trouvées++;
+			} else {nouveauAffiché += mot.textContent[i * 2] + " ";}
+		}
+		mot.textContent = nouveauAffiché.trim();
+		input.value = "";
+		input.focus();
+		if (trouvée) {
+			if (trouvées === longueur) {
+				message.textContent = "Félicitations ! Vous avez deviné le mot.";
+				input.disabled = true;
+				button.disabled = true;
+			} else {message.textContent = "Bonne devinette !";}
+		} else {
+			message.textContent = "Ce n'est pas une bonne lettre. Essayez encore !";
+			essaisRestants--;
+			span.innerText = étapes[6 - essaisRestants];
+			if (essaisRestants === 0) {
+				message.textContent = "Dommage, vous avez perdu. Le mot était : " + secret;
+				input.disabled = true;
+				button.disabled = true;
 			}
 		}
-	);
+	}
 }
